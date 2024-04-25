14f1a433038c95d5086e81ed6b8e69cd6118b3ac
Maximiliano Ortiz <maxiortiz_22@hotmail.com>
2024-04-25 19:53:53 -0300

    Se agregor configuracion de AutoMapper

---
+++ Application/ConfigMapper/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos.Category;
+using AutoMapper;
+using Domain.Entitys;
+
+namespace Application.ConfigMapper
+{
+    public class MapperProfile : Profile
+    {
+
+        public MapperProfile()
+        {
+            CreateMap<Category, DTOCategoryResponse>();
+        }
+    }
+}
